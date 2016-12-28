0d6af86b542b43b35c3fb517550e56156bebc99d
林博仁 <Buo.Ren.Lin@gmail.com>
2016-12-28 17:57:26 +0800

    網站托管服務程式/pop3-user-authentication.c - 套用來源碼美化
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

--- 網站托管服務程式/pop3-user-authentication.c
+++ 網站托管服務程式/pop3-user-authentication.c
@@ -38,48 +38,52 @@ extern int h_errno;
     @arg host_name 域名
     @returns addr.s_addr(?)
     @retval 0 轉換失敗 */
-unsigned long name_resolve(unsigned char *host_name)
+unsigned long name_resolve(unsigned char * host_name)
 {
-        struct in_addr addr;
-        struct hostent *host_ent;
+	struct in_addr addr;
+	struct hostent * host_ent;
 
-        if ((addr.s_addr = inet_addr(host_name)) == -1)
-        {
-                if (!(host_ent = gethostbyname(host_name))){
-                    printf("錯誤：找不到郵件主機(%s)。", host_name);
-                    return (0);
-                }
-                bcopy(host_ent->h_addr, (char *)&addr.s_addr,
-host_ent->h_length);
-        }
-        return (addr.s_addr);
+	if((addr.s_addr = inet_addr(host_name)) == -1)
+	{
+		if(!(host_ent = gethostbyname(host_name))) {
+			printf("錯誤：找不到郵件主機(%s)。", host_name);
+			return (0);
+		}
+
+		bcopy(host_ent->h_addr, (char *)&addr.s_addr,
+		      host_ent->h_length);
+	}
+
+	return (addr.s_addr);
 }
 
 /** @brief 跟伺服器交談的部份
     @arg host_name 域名
     @returns addr.s_addr(?)
     @retval 0 轉換失敗 */
-int interactive(int s, char *cmd) {
-        int bytes;
-        char ret[1024] = "";
+int interactive(int s, char * cmd) {
+	int bytes;
+	char ret[1024] = "";
 
-        if(cmd)
-        if(send(s, cmd, strlen(cmd), 0) == -1) {
-                printf("錯誤：POP3 命令送出失敗。\n");
-                close(s);
-                return(0);
-        }
+	if(cmd)
+		if(send(s, cmd, strlen(cmd), 0) == -1) {
+			printf("錯誤：POP3 命令送出失敗。\n");
+			close(s);
+			return(0);
+		}
 
-        bytes = recv(s, ret, 1024, 0);
-        ret[bytes] = '\0';
-        if(strncasecmp(ret, "+OK", 3) == 0) {
-                ret[0] = '\0';
-        } else {
-                printf("錯誤：POP3 命令執行發生錯誤。\n");
-                close(s);
-                return(0);
-        }
-        return(1);
+	bytes = recv(s, ret, 1024, 0);
+	ret[bytes] = '\0';
+
+	if(strncasecmp(ret, "+OK", 3) == 0) {
+		ret[0] = '\0';
+	} else {
+		printf("錯誤：POP3 命令執行發生錯誤。\n");
+		close(s);
+		return(0);
+	}
+
+	return(1);
 }
 
 /** @brief main 函式 - C/C++ 程式的進入點(entry point)
@@ -88,50 +92,52 @@ int interactive(int s, char *cmd) {
     @returns 此程式要傳回作業系統的結束狀態碼
     @retval 1 身份驗證成功
     @retval 0 身份驗證失敗 */
-int main(int argc,char *argv[]) {
-        char cmd[128];
+int main(int argc, char * argv[]) {
+	char cmd[128];
+	int s;
+	struct sockaddr_in saddr;
+	char * username;
+	char * password;
 
-        int s;
-        struct sockaddr_in saddr;
-        char *username;
-        char *password;
-        if (argc <= 3) {
-            printf("錯誤：參數不正確(host, name, password)。\n");
-            return -1;
-        }
-        username=argv[2];
-        password=argv[3];
+	if(argc <= 3) {
+		printf("錯誤：參數不正確(host, name, password)。\n");
+		return -1;
+	}
 
-        s = socket(PF_INET, SOCK_STREAM, 0);
-        memset(&saddr, 0, sizeof(saddr));
-        saddr.sin_family = PF_INET;
-        saddr.sin_port = htons(PORT_POP3);
-        saddr.sin_addr.s_addr = name_resolve(argv[1]);
+	username = argv[2];
+	password = argv[3];
+	s = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&saddr, 0, sizeof(saddr));
+	saddr.sin_family = PF_INET;
+	saddr.sin_port = htons(PORT_POP3);
+	saddr.sin_addr.s_addr = name_resolve(argv[1]);
 
-        if(connect(s, (struct sockaddr*)&saddr, sizeof(saddr))== 0) {
+	if(connect(s, (struct sockaddr *)&saddr, sizeof(saddr)) == 0) {
+		interactive(s, NULL);
+		sprintf(cmd, "USER %s\r\n", username);
 
-                interactive(s,NULL);
-                sprintf(cmd, "USER %s\r\n", username);
-                if(interactive(s, cmd) == 0){
-                        printf("錯誤: 使用者名稱不存在(%s)\n", username);
-                        close(s);
-                        return(0);
-                }
+		if(interactive(s, cmd) == 0) {
+			printf("錯誤: 使用者名稱不存在(%s)\n", username);
+			close(s);
+			return(0);
+		}
 
-                sprintf(cmd, "PASS %s\r\n", password);
-                if(interactive(s, cmd) == 0){
-                        printf("錯誤: 使用者名稱與密碼組合錯誤(%s)\n", password);
-                        close(s);
-                        return(0);
-                }
-                send(s, "QUIT\r\n", 6, 0);
-                close(s);
+		sprintf(cmd, "PASS %s\r\n", password);
 
-        } else {
-                printf("錯誤：無法連接伺服器\n");
-                close(s);
-                return(0);
-        }
-        printf("正確：認證通過\n");
-        return(1);
+		if(interactive(s, cmd) == 0) {
+			printf("錯誤: 使用者名稱與密碼組合錯誤(%s)\n", password);
+			close(s);
+			return(0);
+		}
+
+		send(s, "QUIT\r\n", 6, 0);
+		close(s);
+	} else {
+		printf("錯誤：無法連接伺服器\n");
+		close(s);
+		return(0);
+	}
+
+	printf("正確：認證通過\n");
+	return(1);
 }
